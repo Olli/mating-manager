0e374da82f936a08f91c9426e508e434b4e62747
Olli <olli17@gmx.net>
2011-02-05 18:06:40 +0100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MatingManager::Application.config.secret_token = '4ad193de1872ba1e4f9f047e82d35dc9a30368e623d17ac4833f2dc615c89d344ecad8e5f9fb3ccab5adf6435414f0d24f3ba575a5a62dce439bb8e191141834'
