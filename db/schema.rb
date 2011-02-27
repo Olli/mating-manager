012fb745f639680338e92777fd7a6f6c08eb44ee
Olli <olli17@gmx.net>
2011-02-27 16:16:15 +0100

    * update of schema data * some basic seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,35 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110205182113) do
+ActiveRecord::Schema.define(:version => 20110220162509) do
+
+  create_table "deliverers", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "mating_apiary_id"
+    t.text     "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "mating_apiaries", :force => true do |t|
     t.string   "name"
-    t.decimal  "lat",         :precision => 10, :scale => 0
-    t.decimal  "lon",         :precision => 10, :scale => 0
+    t.decimal  "lat",        :precision => 10, :scale => 0
+    t.decimal  "lon",        :precision => 10, :scale => 0
     t.text     "address"
-    t.integer  "free_places"
-    t.integer  "used_places"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "mating_units", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "places", :force => true do |t|
+    t.integer  "mating_apiary_id"
+    t.integer  "mating_unit_id"
+    t.integer  "amount",           :default => 1
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -29,6 +49,15 @@ ActiveRecord::Schema.define(:version => 20110205182113) do
     t.text     "value"
     t.text     "interpolations"
     t.boolean  "is_proc",        :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "used_places", :force => true do |t|
+    t.integer  "mating_apiary_id"
+    t.integer  "mating_unit_id"
+    t.integer  "deliverer_id"
+    t.string   "identifier"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+#users
+admin = User.create(:email => 'admin@test.de', :password => 'test', :password_confirmation => 'test')
+deliverer = User.create(:email => 'deliverer@test.de', :password => 'test', :password_confirmation => 'test')
+
+# apiaries
+apiary = MatingApiary.create(:name => "Belegstelle1", :lat => 51, :lon => 13, :address => "Adresse\n Strasse" )
+
+# mating units
+kieler = MatingUnit.create(:name => 'Kieler Begattungskasten')
+miniplus = MatingUnit.create(:name => 'MiniPlus')
+
+apiary.place.create(:mating_unit => kieler, :amount => 5)
+apiary.place.create(:mating_unit => miniplus, :amount => 10)
+
+used_place1 = UsedPlace.create(:mating_apiary => apiary, :deliverer => deliverer, :mating_unit => kieler, :identifier => 'KL1')
+used_place2 = UsedPlace.create(:mating_apiary => apiary, :deliverer => deliverer, :mating_unit => kieler, :identifier => 'KL2')
+used_place3 = UsedPlace.create(:mating_apiary => apiary, :deliverer => deliverer, :mating_unit => miniplus, :identifier => 'MP1')
