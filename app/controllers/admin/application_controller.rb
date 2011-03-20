175ffd8c2f0b611c25f5fb6c0b068034422d44a8
Olli <olli17@gmx.net>
2011-03-20 19:33:02 +0100

    * dont work for some reasons

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,9 +1,7 @@
 class Admin::ApplicationController < ApplicationController
   layout 'admin'
-  before_filter :title
+
+
   
-  protected
-    def title
-      set_title(I18n.t(self.controller_name.to_sym, :scope => :controller))
-    end
+  
 end
