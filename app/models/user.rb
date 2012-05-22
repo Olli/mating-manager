3f88af7bff041c4bc3fc535aa16c8e92e7db5a1f
Olli <olli17@gmx.net>
2012-05-22 19:52:17 +0200

    * reorder mass-assignment attributes * add remember-me to the mass assignment attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,23 @@ class User < ActiveRecord::Base
 
   has_many :deliveries
   belongs_to :country
-  
-  attr_accessible :email, :password, :password_confirmation, 
-                  :first_name, :last_name, :house_number, :street, :zip, :city, :country_id
+
+  attr_accessible :email,
+                  :password,
+                  :password_confirmation,
+                  :first_name,
+                  :last_name,
+                  :house_number,
+                  :street,
+                  :zip,
+                  :city,
+                  :country_id,
+                  :remember_me
 
   def full_name
     "#{first_name} #{last_name}"
   end
-  
+
  protected
   def password_required?
     !persisted? || password.present? || password_confirmation.present?
