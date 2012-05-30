65098990745aa9c02a4096abac41dbb94163ea0f
Olli <olli17@gmx.net>
2012-05-30 13:15:04 +0200

    * tests for attachment in mating unit

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,10 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "paperclip/matchers"
 
 class ActiveSupport::TestCase
+  extend Paperclip::Shoulda::Matchers
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
--- test/unit/mating_unit_test.rb
+++ test/unit/mating_unit_test.rb
@@ -4,4 +4,6 @@ class MatingUnitTest < ActiveSupport::TestCase
   should have_many(:places)
   should have_many(:used_places)
   should have_many(:deliveries)
+  should have_attached_file(:picture)
+  should validate_attachment_size(:picture).less_than(1.megabytes)
 end
