85aac3a1ef6e87dffe0edbdd2aa0555196ebccf9
Olli <olli17@gmx.net>
2012-06-01 15:43:48 +0200

    * optical enhancements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   def link_to_close(element = 'this', linktext = t('remove'))
      link_to_function linktext,
-                      "$(#{element}).parent().fadeOut(function() { $(this).remove() })"
+                      "$(#{element}).parent().fadeOut(function() { $(this).remove() })",
+                      class: 'btn btn-danger'
   end
 
 end
