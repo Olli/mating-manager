a6ceb0b5d2c7c07b3b76b5f7ce3154ae51e794f1
Olli <olli17@gmx.net>
2012-06-02 20:19:11 +0200

    * aasm on new syntac formatted * ruby 1.9 syntax for hashs

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,25 +1,24 @@
 class Delivery < ActiveRecord::Base
   include AASM
-  aasm_column :state
-  aasm_initial_state :requested
-  aasm_state :requested
-  aasm_state :accepted
-  aasm_state :denied
-  aasm_state :archived
+  aasm column: :state do
+    state :requested, initial: true
+    state :accepted
+    state :denied
+    state :archived
 
-  aasm_event :accept do
-    transitions :from => :requested, :to => :accepted
+    event :accept do
+      transitions from: :requested, to: :accepted
+    end
+    event :deny do
+      transitions from: :requested, to: :denied
+    end
+    event :archive do
+      transitions from: [:requested,:denied,:accepted], to: :archived
+    end
+    event :dearchive do
+      transitions from: :archived, to: :requested
+    end
   end
-  aasm_event :deny do
-    transitions :from => :requested, :to => :denied
-  end
-  aasm_event :archive do
-    transitions :from => [:requested,:denied,:accepted], :to => :archived
-  end
-  aasm_event :dearchive do
-    transitions :from => :archived, :to => :requested
-  end
-
   scope :only_new, where(:state => :requested)
   scope :only_accepted, where(:state => :accepted)
   scope :only_denied, where(:state => :denied)
@@ -41,9 +40,9 @@ class Delivery < ActiveRecord::Base
   belongs_to :user
   belongs_to :mating_apiary
   belongs_to :mating_unit
-  validates_presence_of :incomedate, :on => :create
-  validates_presence_of :state
-  validates_presence_of :amount
+  validates  :incomedate, presence: { on: :create }
+  validates  :state, presence: true
+  validates  :amount, presence: true
 
   def change_state(event)
     event = [event.to_sym] & self.aasm_permissible_events_for_current_state
