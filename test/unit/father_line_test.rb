745d364d4e4afa285d172fa14aa7d772460b0c75
Olli <olli17@gmx.net>
2012-06-09 11:47:15 +0200

    * updating fatherline and matingapiary test

--- test/unit/father_line_test.rb
+++ test/unit/father_line_test.rb
@@ -2,8 +2,15 @@ require 'test_helper'
 
 class FatherLineTest < ActiveSupport::TestCase
   should belong_to(:mating_apiary)
+  should belong_to(:user)
   should validate_presence_of(:name)
   should validate_presence_of(:startdate)
   should validate_presence_of(:enddate)
   should validate_presence_of(:race)
+
+  test "should be inactive on create" do
+    a = assets(:carnica)
+    fl = FatherLine.create(a.attributes)
+    assert fl.inactive?
+  end
 end
--- test/unit/mating_apiary_test.rb
+++ test/unit/mating_apiary_test.rb
@@ -4,9 +4,19 @@ class MatingApiaryTest < ActiveSupport::TestCase
   should have_many(:deliveries)
   should have_many(:places)
   should have_many(:used_places)
-  should have_one(:father_line)
+  should have_many(:father_lines)
   should validate_presence_of(:name)
   should validate_presence_of(:address)
   should validate_uniqueness_of(:name)
-  
+
+  test 'fatherline active' do
+    ma = MatingApiary.where(:name => 'Testapiary2').last
+    puts ma.inspect
+    assert_equal 'active', ma.father_line.state
+  end
+
+  test 'fatherline archived' do
+    ma = MatingApiary.where(:name => 'Testapiary1').first
+    assert_equal nil, ma.father_line
+  end
 end
