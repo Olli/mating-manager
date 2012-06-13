9002d6b182f0cb5988a8c08dadd2c77fcc8b964a
Olli <olli17@gmx.net>
2012-06-13 13:02:06 +0200

    * test adapted

--- test/unit/father_line_test.rb
+++ test/unit/father_line_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class FatherLineTest < ActiveSupport::TestCase
   should belong_to(:mating_apiary)
-  should belong_to(:user)
+  should have_one(:user).through(:mating_apiary)
   should validate_presence_of(:name)
   should validate_presence_of(:startdate)
   should validate_presence_of(:enddate)
   should validate_presence_of(:race)
 
   test "should be inactive on create" do
-    a = assets(:carnica)
+    a = FactoryGirl.create(:father_line)
     fl = FatherLine.create(a.attributes)
     assert fl.inactive?
   end
